d38a3a36e42fda5ec60b6c7d6d6ce12f1717ff67
Jesse Romo <jesseromo3@hotmail.com>
2023-02-02 16:15:00 +0200

    added excel writer and frame processing to details.py

--- details.py
+++ details.py
@@ -1,22 +1,69 @@
 import requests
 import pandas
 
-def package_and_send():
-    with open("data_final.txt","r") as file:
-        counter = 0
-        data = []
-        for count, line in enumerate(file):
-            if counter == 99:
-                # send data when we reach the limit(max 100 per request)
-                print(data)
-                # write results in excel?
 
-                # clear data and counter
-                data.clear()
+def process_frame(frame):
+    new_frame = dict()
+    #  add service names to the new frame
+    service_names = dict()
+    for name in frame['serviceNames']:
+        service_names.update({"name": name['value']})
+    new_frame.update({"serviceNames": service_names})
+    # add munincipalities to the new frame
+    muns = {'munincipalities': []}
+    for area in frame['areas']:
+        for mun in area['municipalities']:
+            muns["munincipalities"].append({"name": mun['name'][0]['value']})
+    new_frame.update({"munincipalities": muns})
+    # add service classes to the new frame
+    service_class = {"serviceClasses": []}
+    for cl in frame['serviceClasses']:
+        service_class["serviceClasses"].append(
+            {"name": cl['name'][2]['value'], "code": cl['code']})
+    new_frame.update(service_class)
+     # add id to the frame
+    new_frame.update({"id": frame['id']})
+    # add modified date to the new frame
+    new_frame.update({"modified": frame['modified']})
+    return new_frame
+
+
+def package_and_send():
+    counter = 0
+    total = 0
+    guids_list = []
+    data_frame = []
+
+    with open("data_final.txt", "r") as file:
+        for obj in file:
+            if counter == 99:
+                # make the parameters for the uids
+                guids = ",".join(guids_list)
+                # add parameters to the url
+                url = "https://api.palvelutietovaranto.suomi.fi/api/v11/Service/list?guids={}".format(
+                    guids)
+                # send data when we reach the limit(max 100 per request)
+                response = requests.get(url)
+                # create new dataframe with the data from response.json
+                for objects in response.json():
+                    data_frame.append(process_frame(objects))
+                guids_list.clear()
                 counter = 0
+                total += 1
+                print("adding lines into dataframe, Total frames: "+str(total))
             else:
-                data.append(line.split("'")[3])
+                guids_list.append(str(obj))
                 counter += 1
-        #send remaining data 
+                total += 1
+        # query remaining data
+        guids = ",".join(guids_list)
+        url = "https://api.palvelutietovaranto.suomi.fi/api/v11/Service/list?guids={}".format(
+            guids)
+        response = requests.get(url)
+        for x in response.json():
+            data_frame.append(process_frame(x))
+        data = pandas.DataFrame(data_frame)
+        print("Writing excel file, this might take awhile....")
+        data.to_excel('list_data.xlsx',sheet_name='liikuntapaikka data', index=False)
 
 package_and_send()
--- main.py
+++ main.py
@@ -8,18 +8,11 @@ def clear_file():
         file.write("")
 
 
-def write_file(data):
+def write_file(data, file_name="data.txt"):
     # Append lines in the file
-    with open("data.txt", "a") as file:
-        for count, line in enumerate(data['itemList']):
-            try:
-                # exclude everything before "{" char from the line
-                start_index = str(line).index("{")
-                substring = str(line)[start_index:]
-                file.write(substring+"\n")
-            except(ValueError):
-                # this shouldnt happen but its good to handle a possible error
-                print("error: "+ValueError)          
+    with open(file_name, "a") as file:
+        for obj in data['itemList']:
+            file.write(obj['id'] + "\n")
 
 
 def counts():
@@ -32,14 +25,14 @@ def counts():
 
 def uniq():
     # Check for unique lines in file by inserting every line into a set which only accepts unique values
-    test_set = set()
+    id_set = set()
     with open("data.txt", "r") as file:
-        for count, line in enumerate(file):
-            test_set.add(line)
+        for line in file:
+            id_set.add(line)
     with open("data_final.txt", "w") as final_file:
-        for count, line in enumerate(test_set):
-            final_file.write(line)
-    print("Amount of unique results: "+str(test_set.__len__()))
+        for obj in id_set:
+            final_file.write(obj)
+    print("Amount of unique results: "+str(id_set.__len__()))
 
 
 def write_excel():
@@ -51,7 +44,7 @@ def write_excel():
 
 def main():
     clear_file()
-    codes = ["P27","P27.1"]
+    codes = ["P27", "P27.1"]
     for code in codes:
         url = "https://api.palvelutietovaranto.suomi.fi/api/v11/Service/serviceClass?uri=http%3A%2F%2Furi.suomi.fi%2Fcodelist%2Fptv%2Fptvserclass2%2Fcode%2F{}".format(
             code)
@@ -62,7 +55,7 @@ def main():
             data = response.json()
 
             write_file(data)
-            if len(data['itemList']) < 1000:
+            if len(data['itemList']) < 1000 | response.status_code != 200:
                 # If the number of lines is less than 1000, it means we have reached the last page
                 break
             page += 1
@@ -70,4 +63,31 @@ def main():
     uniq()
     # write_excel()
 
+
+def fetch_total():
+    first_url = "https://api.palvelutietovaranto.suomi.fi/api/v11/ServiceChannel?isVisibleForAll=false&page=1&status=Published"
+    response = requests.get(first_url)
+    response_dict = response.json()
+    last_url = "https://api.palvelutietovaranto.suomi.fi/api/v11/ServiceChannel?isVisibleForAll=false&page={}&status=Published".format(
+        response_dict['pageCount'])
+    last_response = requests.get(last_url)
+    last_dict = last_response.json()
+    for count, obj in enumerate(last_dict['itemList']):
+        pass
+    print("Total number of objects in ServiceChannels: " +
+          str(response_dict['pageCount'] * 1000 + count))
+
+
+def fetch_details():
+    uid = "e3658c00-02c9-4fd4-8062-f89c9e9dfeb3"
+    url = "https://api.palvelutietovaranto.suomi.fi/api/v11/ServiceChannel/{}".format(
+        uid)
+    response = requests.get(url)
+    data = response.json()
+    with open("details.xlsx", "w") as file:
+        for x in data:
+            print(x)
+
 main()
+# fetch_details()
+# fetch_total()
